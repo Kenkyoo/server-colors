09936b41d95b3d43b52d86696b986aa86ff5509e
Kenkyoo <124638519+Kenkyoo@users.noreply.github.com>
2024-11-28 07:40:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require("fs");
+const cors = require('cors')
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    let data = fs.readFileSync('../../colornames.json');
+    let colorNames = JSON.parse(data);
+    res.json(colorNames);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
